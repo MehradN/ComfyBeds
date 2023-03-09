e5167f53cdd8ff84d2469d1eafece6c1311ba2e9
Mehrad Nayyeri <mehrad.nayyeri@zohomail.com>
2023-03-09 09:08:45 +0330

    Initial commit

---
+++ src/main/java/ir/mehradn/comfybeds/ComfyBedsMod.java
@@ -0,0 +1,13 @@
+package ir.mehradn.comfybeds;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComfyBedsMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("comfy-beds");
+
+	public void onInitialize() {
+	}
+}
